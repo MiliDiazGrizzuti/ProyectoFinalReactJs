b3cb5a851fa58ae7a2f7555c819e3b3d7d6669f1
MiliDiazGrizzuti <milagros.diazgrizzuti@hotmail.com>
2023-06-09 17:52:04 -0300

    imagenes

--- src/Productos.js
+++ src/Productos.js
@@ -1,15 +1,10 @@
-import Vestido from './Imagenes/Vestido.jpeg';
-import Gorra from './Imagenes/Gorra.jpeg';
-import Mantel from './Imagenes/mantel.jpeg';
-
-
 const products = [
     {
         id: '1',
         name: 'Vestido Paris',
         price: 12000,
         category: 'indumentaria',
-        img: <img src={Vestido} alt="Productos" className='Vestido'></img>,
+        img: '../Imagenes/Vestido.jpeg',
         stock: 15,
         despription: 'Vestido de lino'
     },
@@ -18,7 +13,7 @@ const products = [
         name: 'Gorra África',
         price: 6000,
         category: 'Accesorios',
-        img: <img src={Gorra} alt="Productos" className='Gorra'></img>,
+        img: '../Imagenes/Gorra.jpeg',
         stock: 22,
         despription: 'Gorra beige'
     },
@@ -27,7 +22,7 @@ const products = [
         name: 'Mantel Santorini',
         price: 10000,
         category: 'Home Deco',
-        img: <img src={Mantel} alt="Productos" className='Mantel'></img>,
+        img: '../Imagenes/Mantel.jpeg',
         stock: 10,
         despription: 'Mantel de lino'
     },
